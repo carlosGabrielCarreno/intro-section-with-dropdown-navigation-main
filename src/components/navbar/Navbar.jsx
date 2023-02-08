018631e9d31e170d1bccb6401a92a38e34019fd5
carlosgabrielcarreno <carrenocarlosgabriel@gmail.com>
2023-02-08 17:11:47 -0300

    add changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,88 +1,92 @@
-import { useEffect, useState } from 'react';
-import { BurguerButton } from '../BurguerButton';
-import logo from '../../images/logo.svg';
-import todoList from '../../images/icon-todo.svg';
-import calendar from '../../images/icon-calendar.svg';
-import reminders from '../../images/icon-reminders.svg';
-import planning from '../../images/icon-planning.svg';
-import './_navbar.scss';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import { BurguerButton } from "../BurguerButton";
+import logo from "../../images/logo.svg";
+import todoList from "../../images/icon-todo.svg";
+import calendar from "../../images/icon-calendar.svg";
+import reminders from "../../images/icon-reminders.svg";
+import planning from "../../images/icon-planning.svg";
+import "./_navbar.scss";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const Navbar = () => {
-	const [showMobileMenu, setShowMobileMenu] = useState(false);
-	const [windowSize, setWindowSize] = useState({
-		width: window.innerWidth,
-		height: window.innerHeight,
-	});
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-	useEffect(() => {
-		function handleResize() {
-			setWindowSize({
-				width: window.innerWidth,
-				height: window.innerHeight,
-			});
-		}
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
 
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
-	return (
-		<header className="container">
-			<div className="wrapper">
-				<img src={logo} className="logotipo" alt="logo mark" />
-				{/*  */}
-				{windowSize.width > 960 ? (
-					<>
-						<nav className={`links-desktop`}>
-							<ul>
-								<div className="calendar">Calendar</div>
-								<div className="reminders">Reminders</div>
-								<div className="planning">Planning</div>
-								<div className="company">Company</div>
-								<div className="carrers">Carrers</div>
-								<div className="about">About</div>
-								<div className="container-buttons">
-									<button className="login">Login</button>
-									<button className="register">Register</button>
-								</div>
-							</ul>
-						</nav>
-					</>
-				) : (
-					<></>
-				)}
-				{/*  */}
-				<div
-					className="mobileIcon"
-					onClick={() => setShowMobileMenu(!showMobileMenu)}
-				>
-					{showMobileMenu ? <FaTimes /> : <FaBars />}
-				</div>
-			</div>
-			{showMobileMenu ? (
-				<>
-					<nav className={`links ${showMobileMenu ? 'open' : ''}`}>
-						<ul>
-							<div className="calendar">Calendar</div>
-							<div className="reminders">Reminders</div>
-							<div className="planning">Planning</div>
-							<div className="company">Company</div>
-							<div className="carrers">Carrers</div>
-							<div className="about">About</div>
-							<div className="container-buttons">
-								<button className="login">Login</button>
-								<button className="register">Register</button>
-							</div>
-						</ul>
-					</nav>
-				</>
-			) : (
-				<></>
-			)}
-		</header>
-	);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <header className="container">
+      <div className="wrapper">
+        <img src={logo} className="logotipo" alt="logo mark" />
+        {/*  */}
+        {windowSize.width > 960 ? (
+          <>
+            <nav className={`links-desktop`}>
+              <ul>
+                <div className="calendar">Calendar</div>
+                <div className="reminders">Reminders</div>
+                <div className="planning">Planning</div>
+                <div className="company">Company</div>
+                <div className="carrers">Carrers</div>
+                <div className="about">About</div>
+                <div className="container-buttons">
+                  <button className="login">Login</button>
+                  <button className="register">Register</button>
+                </div>
+              </ul>
+            </nav>
+          </>
+        ) : (
+          <></>
+        )}
+        {/*  */}
+        <div
+          className="mobileIcon"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          {showMobileMenu ? <FaTimes /> : <FaBars />}
+        </div>
+      </div>
+      {showMobileMenu ? (
+        <>
+          <nav
+            className={`links ${
+              showMobileMenu && windowSize.width < 960 ? "open" : ""
+            }`}
+          >
+            <ul>
+              <div className="calendar">Calendar</div>
+              <div className="reminders">Reminders</div>
+              <div className="planning">Planning</div>
+              <div className="company">Company</div>
+              <div className="carrers">Carrers</div>
+              <div className="about">About</div>
+              <div className="container-buttons">
+                <button className="login">Login</button>
+                <button className="register">Register</button>
+              </div>
+            </ul>
+          </nav>
+        </>
+      ) : (
+        <></>
+      )}
+    </header>
+  );
 };
 
 // export const Navbar = () => {
